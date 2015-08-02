23c71cc1755230485f9c058be1a63889ca57c0d0
AmitaiB <blickstein@gmail.com>
2015-08-02 17:23:13 -0400

    merged, finally

---
+++ LearnYou3/LY2Constants.h
@@ -0,0 +1,18 @@
+//
+//  LY2Constants.h
+//  LearnYou3
+//
+//  Created by Amitai Blickstein on 8/2/15.
+//  Copyright (c) 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LY2Constants : NSObject
+
+extern NSString * const GITHUB_CLIENT_ID;
+extern NSString * const GITHIB_CLIENT_SECRET;
+extern NSString * const GITHUB_PAC_TOKEN;
+extern NSString * const REST_API_KEY;
+
+@end
