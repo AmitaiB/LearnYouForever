59db0c934781b45e2dd35652f245b90109d2a561
AmitaiB <blickstein@gmail.com>
2015-08-02 17:15:54 -0400

    squash me

---
+++ LearnYou3/LY3DataStoreClient.h
@@ -0,0 +1,13 @@
+//
+//  LY3DataStoreClient.h
+//  
+//
+//  Created by Amitai Blickstein on 8/2/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LY3DataStoreClient : NSObject
+
+@end
---
+++ LearnYou3/LY3DataStoreClient.m
@@ -0,0 +1,13 @@
+//
+//  LY3DataStoreClient.m
+//  
+//
+//  Created by Amitai Blickstein on 8/2/15.
+//
+//
+
+#import "LY3DataStoreClient.h"
+
+@implementation LY3DataStoreClient
+
+@end
