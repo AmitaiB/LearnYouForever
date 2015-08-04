44fe923cd74a4be47f4652bc4cc576b601252bd4
AmitaiB <blickstein@gmail.com>
2015-08-04 07:10:44 -0400

    prettifies loginVC

--- LearnYou3/LoginViewController.m
+++ LearnYou3/LoginViewController.m
@@ -34,32 +34,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    NSLog(@"Viewdidload called");
     // Do any additional setup after loading the view.
-    
-   
+    NSLog(@"viewDidLoad...");
+        //Just a little demonstration code to show what the DLIL cocoapod cleaned up:
+//    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(50, 50, 100, 100)];
+//    imageView.image = [UIImage imageWithCIImage:[CIImage imageWithContentsOfURL:[NSURL URLWithString:@"https://www.google.com/images/srpr/logo11w.png"]]];
+//    [self.view addSubview:imageView];
 }
 
 
 -(void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    
-    
-    NSLog(@"View Will appear!!");
-    
-    
+    NSLog(@"viewWillAppear...");
     
     if ([AFOAuthCredential retrieveCredentialWithIdentifier:@"githubOAuthToken"]) {
-        
         NSLog(@"In if statement of retrieveCredential");
             [self performSegueWithIdentifier:@"logInToMasterSegueID" sender:nil];
     }
     
         //Notify this instance of the view controller and call method.
+        //...Why??
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleApplicationOpenedForURL:) name:@"ApplicationOpenedForURL" object:nil];
 }
-
+    //More NSNotificationCenter stuff...
 -(NSString *)firstValueForQueryItemNamed:(NSString *)name inURL:(NSURL *)url {
     
     NSURLComponents *urlComponents = [NSURLComponents componentsWithURL:url resolvingAgainstBaseURL:nil];
@@ -76,78 +73,57 @@
     return nil;
 }
 
-
+    //And yet more NSNotificationCenter stuff...
 -(void)handleApplicationOpenedForURL:(NSNotification *)notification
 {
     
     NSURL *url = notification.userInfo[@"url"];
     
 //    [[NSNotificationCenter defaultCenter] postNotificationName:@"ApplicationOpenedForURL" object:nil userInfo:@{@"url":url}];
-    
     NSString *code = [self firstValueForQueryItemNamed:@"code" inURL:url];
     NSLog(@"Opened from URL %@", url);
-    
 }
 
     ///AFOAuth2Manager stores passwords etc. in the Keychain.
 - (IBAction)githubButtonWasTapped:(id)sender {
 //    AFOAuth2Manager *oAuthManager = [self githubOAuth2ManagerCreate];
     
-    GitHubOAuthController *oAuthController = [[GitHubOAuthController alloc] initWithClientId:GITHUB_CLIENT_ID
-                                                                                clientSecret:GITHIB_CLIENT_SECRET
-                                                                                       scope:@"repo"
-                                                                                     success:^(NSString *accessToken, NSDictionary *raw) {
-                                                                                         NSLog(@"access token: %@ \nraw: %@", accessToken, raw);
-        AFOAuthCredential *credential = [AFOAuthCredential credentialWithOAuthToken:raw[@"access_token"]
-                                                                          tokenType:raw[@"token_type"]];
-        [AFOAuthCredential storeCredential:credential withIdentifier:@"githubOAuthToken"];
+    GitHubOAuthController *oAuthController = [[GitHubOAuthController alloc] initWithClientId:GITHUB_CLIENT_ID clientSecret:GITHIB_CLIENT_SECRET scope:@"repo" success:^(NSString *accessToken, NSDictionary *raw) {
+                NSLog(@"access token: %@ \nraw: %@", accessToken, raw);
+                AFOAuthCredential *credential = [AFOAuthCredential credentialWithOAuthToken:raw[@"access_token"] tokenType:raw[@"token_type"]];
+                [AFOAuthCredential storeCredential:credential withIdentifier:@"githubOAuthToken"];
+    } failure:nil];
 
-                                                                                     } failure:nil];
-    
     [oAuthController showModalFromController:self];
 }
-
+/**
+ *  Using Parse as a Backend Persistent Data Store, the array is queried
+ *  -- returning a URL (as NSString) for a random octocat image from the 'octodex' (Github). This unofficial API was created by Import.io.
+ *  @param sender A button overlaying the image of Octocat.
+ */
 - (IBAction)invisibleButtonTapped:(id)sender {
     PFQuery *octoQuery = [PFQuery queryWithClassName:@"octodex"];
     [octoQuery getObjectInBackgroundWithId:@"WIENIhX2vV" block:^(PFObject *object, NSError *error) {
-        NSLog(@"PFObject (response object): %@", object);
+        NSLog(@"PFObject (response object): %@", object.allKeys);
         [self randomizeOctocat:object];
     }];
     
 }
 
 -(void)randomizeOctocat:(PFObject*)object {
-    NSLog(@"PFObject = %@", object);
+    NSLog(@"PFObject.allKeys = %@", object.allKeys);
     NSArray *octodex = object[@"octocatURLs"];
-    NSLog(@"octodex = %@", octodex);
+    NSLog(@"octodex.count = %@", [@(octodex.count) stringValue]);
     NSUInteger random = (NSUInteger)[self randomFloatBetweenNumber:0 andNumber:octodex.count - 1];
     NSString *randomOCatURL = octodex[random];
     NSLog(@"Random0CatURL: %@", randomOCatURL);
     [self.octocatDLImageView displayImageFromUrl:randomOCatURL];
-    
-//    DLImageLoader *imageLoader = [DLImageLoader sharedInstance];
-//    [imageLoader displayImageFromUrl:randomOCatURL imageView:self.octocatImageView];
-//   [imageLoader loadImageFromUrl:randomOCatURL completed:^(NSError *error, UIImage *image) {
-//       [self assignNewImage:image ToView:self.octocatImageView];
-//   }];
-    
-    
-//    self.octocatImage.image = [UIImage image]
 }
 
-    //      arc4random is 0 to it's MAX. divided by it's MAX gives you a percentage. Multiply that by the range, and then add that to the original quantity...
+    //      arc4random is 0 to its MAX → divided by that MAX normalizes the range (i.e., from 0.0 - 1.0), essentially a percentage. Multiply that by the range, and then add that to the original quantity...
 -(CGFloat)randomFloatBetweenNumber:(CGFloat)minRange andNumber:(CGFloat)maxRange {
     return ((float)arc4random() / ARC4RANDOM_MAX) * (maxRange - minRange); //+ minRange;
 }
 
--(void)assignNewImage:(UIImage*)image ToView:(UIImageView*)imageView {
-    imageView.image = image;
-    [imageView reloadInputViews];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
